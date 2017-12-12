f06e0e840ab8a798c7a7dbdbf58c829aff1ebf9c
kn1cht <ito.turukasiwa@gmail.com>
2017-12-13 01:45:10 +0900

    Add retry loop

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 'use strict';
 require('dotenv').config();
+require('date-utils');
 const config = require('config');
 const cron = require('node-cron');
 const https = require('https');
 const portscanner = require('portscanner');
 const twitter = require('twitter');
 const util = require('util');
+
+const MAX_RETRY = 3;
 const bot = new twitter ({
   consumer_key        : process.env.CONSUMER_KEY,
   consumer_secret     : process.env.CONSUMER_SECRET,
@@ -20,7 +23,6 @@ for(const pool of config.pools) {
 
 /*** check MPOS API reachability ***/
 const checkAPI = async(uri) => {
-  console.info(uri);
   const data = await new Promise((resolve) => {
     https.get(uri, (res) => {
       res.setEncoding('utf8');
@@ -30,8 +32,8 @@ const checkAPI = async(uri) => {
     }).on('timeout', () => {
       resolve({ error : 'Timeout' });
     }).on('error', (err) => {
-      console.error(err);
-    }).setTimeout(30000).end();
+      console.debug(err);
+    }).setTimeout(2000).end();
   });
   if(!data.error) {
     try {
@@ -47,7 +49,7 @@ const checkAPI = async(uri) => {
 
 /*** check Stratum Port reachability ***/
 const checkStratum = async(host, port) => {
-  const portStatus = await portscanner.checkPortStatus(port, { host, timeout : 2000 });
+  const portStatus = await portscanner.checkPortStatus(port, { host, timeout : 1000 });
   return portStatus === 'open' ? true : false;
 };
 
@@ -57,18 +59,29 @@ const postTweet = (status) => {
   }); 
 };
 
-const main = async() => {
+const checkCurrentStatus = async() => {
   for(const pool of config.pools) {
+    console.info(`[${new Date()}] Checking ${pool.name}...`);
     const previous = previousStatus[pool.id];
-    const api = await checkAPI(pool.url + config.apipath);
-    const stratum = await checkStratum(pool.stratum.host, pool.stratum.port);
-    if(previous.api !== api || previous.stratum !== stratum) {
-      previousStatus[pool.id] = { api, stratum };
+    const current = { api : false, stratum : false };
+    for(let i = 0; i < MAX_RETRY; ++i) {
+      current.api = await checkAPI(pool.url + config.apipath);
+      if(current.api) { break; }
+    }
+    for(let i = 0; i < MAX_RETRY; ++i) {
+      current.stratum = await checkStratum(pool.stratum.host, pool.stratum.port);
+      if(current.stratum) { break; }
+    }
+
+    if(previous.api !== current.api || previous.stratum !== current.stratum) {
+      previousStatus[pool.id] = { api : current.api, stratum : current.stratum };
       let text = '';
-      if(!api || !stratum) { text += `【鯖落ち】「${pool.name}」に接続障害の可能性\n`; }
+      if(!current.api || !current.stratum) { text += `【鯖落ち】「${pool.name}」に接続障害の可能性\n`; }
       else { text += `【復旧】「${pool.name}」が復帰しました\n`; }
-      text += `Webダッシュボード: ${api ? '\u2705 正常' : '\u26a0 停止'}\n`;
-      text += `Stratumポート: ${stratum ? '\u2705 正常' : '\u26a0 停止'}\n`;
+      text += `${pool.url}\n`;
+      text += `Webダッシュボード: ${current.api ? '\u2705 正常' : '\u26a0 停止'}\n`;
+      text += `Stratumポート: ${current.stratum ? '\u2705 正常' : '\u26a0 停止'}\n`;
+      text += `(${(new Date()).toFormat('YYYY/MM/DD HH24:MI:SS')} JST)\n`;
       text += '#bitzeny #ZNY';
       console.info(text);
       postTweet(text);
@@ -76,4 +89,4 @@ const main = async() => {
   }
 };
 
-cron.schedule('0-59/5 * * * *', () => { main(); });
+cron.schedule('0-59/5 * * * *', () => { checkCurrentStatus(); });
