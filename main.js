de8c77a7abb3f22753ba42ab472dd61553c04a30
kn1cht <ito.turukasiwa@gmail.com>
2017-12-16 06:32:28 +0900

    use request module

--- main.js
+++ main.js
@@ -3,10 +3,9 @@ require('dotenv').config();
 require('date-utils');
 const config = require('config');
 const cron = require('node-cron');
-const https = require('https');
 const portscanner = require('portscanner');
+const request = require('request');
 const twitter = require('twitter');
-const util = require('util');
 
 const MAX_RETRY = 3;
 const bot = new twitter ({
@@ -24,16 +23,10 @@ for(const pool of config.pools) {
 /*** check MPOS API reachability ***/
 const checkAPI = async(uri) => {
   const data = await new Promise((resolve) => {
-    https.get(uri, (res) => {
-      res.setEncoding('utf8');
-      let body = '';
-      res.on('data', (data) => { body += data; });
-      res.on('end', () => { resolve({ body }); });
-    }).on('timeout', () => {
-      resolve({ error : 'Timeout' });
-    }).on('error', (err) => {
-      resolve({ error : err });
-    }).setTimeout(config.timeout.api || 1000).end();
+    request.get(uri, { timeout : config.timeout.api || 1000 }, (error, response, body) => {
+      if(error) { resolve({ error }); }
+      else { resolve({ body }); }
+    });
   });
   if(!data.error) {
     try {
@@ -68,11 +61,11 @@ const checkCurrentStatus = async() => {
     console.info(`[${new Date()}] Checking ${pool.name}...`);
     const previous = previousStatus[pool.id];
     const current = { api : false, stratum : false };
-    for(let i = 0; i < MAX_RETRY; ++i) {
+    for(let retry = 0; retry < MAX_RETRY; ++retry) {
       current.api = await checkAPI(pool.url + config.apipath);
       if(current.api) { break; }
     }
-    for(let i = 0; i < MAX_RETRY; ++i) {
+    for(let retry = 0; retry < MAX_RETRY; ++retry) {
       current.stratum = await checkStratum(pool.stratum.host, pool.stratum.port);
       if(current.stratum) { break; }
     }
@@ -96,21 +89,21 @@ const tweetAllStatus = () => {
   let text = '【定期】プール稼働状況\n';
   for(const pool of config.pools) {
     const status = previousStatus[pool.id];
-    text += `${pool.shortname || pool.name} `
+    text += `${pool.shortname || pool.name} `;
     if(status.api && status.stratum) { text += '\u2705'; }
     else if(status.stratum) { text += '\u26a0(Web)'; }
     else if(status.api) { text += '\u26a0(Stratum)'; }
     else { text += '\u26a0(Web/Stratum)'; }
-    text += `\n`;
+    text += '\n';
   }
   text += `(${(new Date()).toFormat('YYYY/MM/DD HH24:MI:SS')} JST)\n`;
   text += '#bitzeny #ZNY';
   console.info(text);
   postTweet(text);
-}
+};
 
-if(process.env.DEBUG) { 
-  checkCurrentStatus(); 
+if(process.env.DEBUG) {
+  checkCurrentStatus();
   tweetAllStatus();
 }
 else {
